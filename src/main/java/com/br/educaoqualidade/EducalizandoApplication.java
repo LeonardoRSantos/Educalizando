fc11fb733d63d52b85353287e6236e1a400f9a00
LeonardoRSantos <leonardoworkbench@gmail.com>
2022-04-26 22:56:51 -0300

    Projeto Educalizando 1.0 V

---
+++ src/main/java/com/br/educaoqualidade/EducalizandoApplication.java
@@ -0,0 +1,13 @@
+package com.br.educaoqualidade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EducalizandoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EducalizandoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/educaoqualidade/EducalizandoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.educaoqualidade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EducalizandoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
