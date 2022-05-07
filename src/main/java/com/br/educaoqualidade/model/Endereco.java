2356838dfca7202f12f57eef0ae70d4897ed0eab
LeonardoRSantos <leonardoworkbench@gmail.com>
2022-05-07 13:06:34 -0300

    Adicionando Anotações JPA nas classes

--- src/main/java/com/br/educaoqualidade/model/Endereco.java
+++ src/main/java/com/br/educaoqualidade/model/Endereco.java
@@ -1,12 +1,26 @@
 package com.br.educaoqualidade.model;
 
+
+import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+@Entity
 public class Endereco {
 
+    @Id
     private Long id;
     private String numero;
     private String cidade;
     private String uf;
     private String cep;
-    private Aluno aluno;
-    private Instituicao instituicao;
+
+    @OneToMany(mappedBy = "aluno", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private Set<@NotEmpty Aluno> alunos = new LinkedHashSet<>();
+
+    @OneToMany(mappedBy = "instituicao", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private Set<@NotEmpty Instituicao> instituicaos = new LinkedHashSet<>();
+
+
 }
