94276f9a9a64707a7668729141d22721484f9cd3
EricSM99 <ericmaciel999@gmail.com>
2022-05-08 12:57:02 -0300

    Implementando Serializable na class Universidade

--- src/main/java/com/br/educaoqualidade/model/Universidade.java
+++ src/main/java/com/br/educaoqualidade/model/Universidade.java
@@ -1,7 +1,12 @@
 package com.br.educaoqualidade.model;
 
+import java.io.Serializable;
+
 import com.br.educaoqualidade.model.Instituicao;
 
-public class Universidade extends Instituicao {
+public class Universidade extends Instituicao implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+	
     private Long id;
 }
